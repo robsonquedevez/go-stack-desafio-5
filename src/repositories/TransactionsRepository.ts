ae6f220708c0d0b572218c4193932eb8eb9de0cc
Robson Quedevez de Alcantara <robson.quedevez@grupoceral.local>
2020-06-23 20:16:45 -0300

    implemented

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,64 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public getBalance(): Balance {
+    const balance = this.transactions.reduce((accumulator: Balance, transaction: Transaction) => {
+      switch(transaction.type){
+        case "income":
+          accumulator.income += transaction.value;
+          break;
+        case "outcome":
+          accumulator.outcome += transaction.value;
+          break;
+        default:
+          break;
+      }
+
+      accumulator.total = accumulator.income - accumulator.outcome;
+
+      return accumulator;
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    });
+
+    return balance;
+  }
+
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const balanceValidator = this.getBalance();
+
+    if(type === "outcome" && balanceValidator.total < value ){
+      throw Error('Insufficient balance for transaction');
+    }
+
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
